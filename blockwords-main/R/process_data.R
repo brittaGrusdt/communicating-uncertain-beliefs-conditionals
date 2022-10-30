b88f283dce9685bb7e1da77b670cc803b13952cf
Britta Grusdt <bgrusdt@gmail.com>
2022-10-30 20:50:01 +0100

    some more tidying up + code refactoring

--- blockwords-main/R/process_data.R
+++ blockwords-main/R/process_data.R
@@ -3,6 +3,7 @@ library(tidyverse)
 source("R/utils.R")
 source("R/utils-exp2.R")
 source("R/Dirichlet-fits.R")
+source(here("R", "analysis-utils.R"))
 
 # Setup -------------------------------------------------------------------
 N_trials = list(train = 14 + 3 + 10, 
@@ -10,15 +11,37 @@ N_trials = list(train = 14 + 3 + 10,
                 color_vision = 6,
                 slider_choice = 10, 
                 attention_check = 3);
-data_fn <- "raw_results_54_blockwords-main_BG.csv"
 data_dir = here("data")
+data_fn <- "raw_results_54_blockwords-main_BG.csv"
 path_to_raw_data = paste(data_dir, data_fn, sep=.Platform$file.sep)
 
 result_dir <- here("data", "formatted-all")
 if(!dir.exists(result_dir)) dir.create(result_dir, recursive=TRUE);
 
+# create folder for cleaned data
+cleaned_dir = here("data", "formatted-cleaned")
+if(!dir.exists(cleaned_dir)) dir.create(cleaned_dir)
+
 # Processing --------------------------------------------------------------
 data <- process_data(path_to_raw_data, result_dir, N_trials)
+
+# Check comments for cleaning data  ---------------------------------------
+df.comments = data$comments %>% filter(!is.na(comments) & comments!="")
+df.comments$comments
+# filter based on these comments:
+out.comments = df.comments %>%
+  filter(comments == "I apologize because the first couple of graphics did not appear properly, so I picked any answer. The rest of the study went fine." |
+           comments == "The descriptions on this quiz were terrible and confusing. Non native English speakers will have a lot of trouble." |
+           comments == "This was hard to understand")
+
+# remove all trials for these participants
+test_trials = data$test$id %>% unique()
+df.out = out.comments %>% dplyr::select(prolific_id) %>% 
+  add_column(id = rep(list(test_trials), nrow(out.comments))) %>%
+  unnest(c(id)) %>%
+  group_by(prolific_id)
+# write_csv(df.out, here("data", "out_by_comments.csv"))
+
 
 # Save data in different formats ------------------------------------------
 # PE-Task data
@@ -53,10 +76,8 @@ joint.human.orig = left_join(
   exp2.human %>% dplyr::select(-RT, -custom_response),
   by = c("prolific_id", "id", "utterance")
 )
-save_data(joint.human.orig, 
-          paste(result_dir, "human-exp1-orig-exp2.rds", sep = FS))
-save_data(joint.human.smooth,
-          paste(result_dir, "human-exp1-smoothed-exp2.rds", sep = FS))
+save_data(joint.human.orig, paste(result_dir, "human-exp1-orig-exp2.rds", sep = FS))
+save_data(joint.human.smooth,paste(result_dir, "human-exp1-smoothed-exp2.rds", sep = FS))
 
 # save count + frequency of created utterances per stimulus
 human.exp2.avg = task2_avg_per_stimulus(result_dir)
@@ -65,26 +86,22 @@ write_csv(human.exp2.avg, paste(result_dir, "behavioral-avg-task2.csv", sep=FS))
 df2.chunked = human.exp2.avg %>% chunk_utterances()
 df2.chunked.avg = df2.chunked %>% group_by(id, utterance) %>% 
   summarize(count = sum(count), ratio = sum(ratio), .groups="drop_last")
-write_csv(df2.chunked.avg, 
-          paste(result_dir, "behavioral-avg-task2-chunked.csv", sep = FS))
+write_csv(df2.chunked.avg, paste(result_dir, "behavioral-avg-task2-chunked.csv", sep = FS))
 
 # Participants info
 save_data(data$info, paste(result_dir, "participants-info.rds", sep = FS))
 
-# Quality of data in slider ratings:
+# compute + save quality of data in slider ratings:
 df = exp1.human.orig %>% rename(response=human_exp1) %>%
   dplyr::select(-utterance) %>% filter(!is.na(question))
 prior.quality = distancesResponses(df)
-save_data(prior.quality, 
-          paste(result_dir, "test-data-prior-quality.rds", sep = FS))
+save_data(prior.quality, paste(result_dir, "test-data-prior-quality.rds", sep = FS))
 
 
-# look at comments/time/quality and specify in results_joint_experiment.Rmd
-# participants to be excluded,then run filter-function
-source(here("R", "analysis-utils.R"))
-df.filtered = filter_data(result_dir, 
-                          here("data", "out_by_comments.csv"),
-                          here("data", "out_by_quality.csv"))
+# Filter data according to predefined criteria ----------------------------
+df.cleaned = filter_data(result_dir, cleaned_dir, here("data", "out_by_comments.csv"))
 
+# format data for model ---------------------------------------------------
+process_and_save_data_for_model(cleaned_dir)
 
 
